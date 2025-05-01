0be814faec8d220bf2e33109965a4ec7b195bc9c
Aref <aref.unlimited.84@gmail.com>
2025-05-01 19:05:18 +0330

    Media in module-info.java added.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.youtube_graphic {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     opens com.example.youtube_graphic to javafx.fxml;
     opens Controller to javafx.fxml;
